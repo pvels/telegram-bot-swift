e31fd4c635a0f96279f5218bc5ee7e5be2ee1732
Pavel Gerashchenko <p.gerashchenko@gmail.com>
2018-01-26 02:00:17 +0300

    swift package manages fixs

--- Package.swift
+++ Package.swift
@@ -4,9 +4,8 @@ let package = Package(
     name: "TelegramBotSDK",
     targets: [Target(name: "TelegramBotSDK")],
     dependencies: [
-	.Package(url: "https://github.com/zmeyc/CCurl.git", majorVersion: 0),
+        .Package(url: "https://github.com/zmeyc/CCurl.git", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
-//        .Package(url: "https://github.com/smud/ScannerUtils.git", majorVersion: 1)
     ]
 )
 
